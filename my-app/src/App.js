edc6313b8d9f756f6a16117e577193fe40e2c993
sabuhi <msebuhi040@gmail.com>
2023-07-04 18:22:17 +0400

    I created My first React app

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Components/Header';
+
+  class App extends React.Component{
+  render() {
+
+     return (
+     <>
+     <Header/>
+     </>
+     );
+   }
+}
+export default App;
